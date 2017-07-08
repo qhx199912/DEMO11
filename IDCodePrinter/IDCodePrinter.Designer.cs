453560cc113f655a57074a5507c5f39013454bc5
shen.jz <shen.jz@dt1.biz>
2017-07-08 20:15:16 +0800

    master

---
+++ IDCodePrinter/IDCodePrinter.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace IDCodePrinter
+{
+    partial class IDCodePrinter
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(381, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(244, 61);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(381, 115);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(294, 249);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "IDCodePrinter.Report.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(696, 13);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(536, 466);
+            this.reportViewer1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(265, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 74);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "打印";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // IDCodePrinter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1236, 609);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "IDCodePrinter";
+            this.Text = "IDCodePrinter";
+            this.Load += new System.EventHandler(this.IDCodePrinter_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ IDCodePrinter/IDCodePrinter.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.Reporting.WinForms;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Drawing.Printing;
+using ByteMatrix = com.google.zxing.common.ByteMatrix;
+using EAN8Writer = com.google.zxing.oned.EAN8Writer;
+using com.google.zxing;
+using DataMatrix.net;
+using S7;
+
+namespace IDCodePrinter
+{
+    public partial class IDCodePrinter : Form
+    {
+        public IDCodePrinter()
+        {
+            InitializeComponent();
+
+            try
+            {
+                pictureBox2.Image = Encode_DM("# 3Q0.915.590.F#131355100#", 5, 10);
+                pictureBox1.Image = Encode_EAN_13("12345678");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static Bitmap Encode_DM(string content, int moduleSize = 5, int margin = 5)
+        {
+            DmtxImageEncoderOptions opt = new DmtxImageEncoderOptions();
+            opt.ModuleSize = moduleSize;
+            opt.MarginSize = margin;
+            //opt.SizeIdx = DmtxSymbolSize.DmtxSymbol40x40;
+            //opt.Scheme = DmtxScheme.DmtxSchemeText;
+
+            DmtxImageEncoder encoder = new DmtxImageEncoder();
+
+            Bitmap bm = encoder.EncodeImage(content, opt);
+            return bm;
+        }
+
+        public static Bitmap Encode_EAN_13(string content)
+        {
+            EAN8Writer ean8w = new EAN8Writer();
+            ByteMatrix byteMatrix = ean8w.encode(content, BarcodeFormat.EAN_8, 300, 200);
+            Bitmap bitmap = ByteMatrixToBitmap(byteMatrix);
+
+            return bitmap;
+        }
+
+        public static Bitmap ByteMatrixToBitmap(ByteMatrix matrix)
+        {
+            int width = matrix.Width;
+            int height = matrix.Height;
+            Bitmap bmap = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    bmap.SetPixel(x, y, matrix.get_Renamed(x, y) != -1 ? ColorTranslator.FromHtml("0xFF000000") : ColorTranslator.FromHtml("0xFFFFFFFF"));
+                }
+            }
+            return bmap;
+        }
+
+        private byte[] BitmapToBytes(Bitmap Bitmap)
+        {
+            MemoryStream ms = null;
+            try
+            {
+                ms = new MemoryStream();
+                Bitmap.Save(ms, ImageFormat.Gif);
+                byte[] byteImage = new Byte[ms.Length];
+                byteImage = ms.ToArray();
+                return byteImage;
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                ms.Close();
+            }
+        }
+
+        private void IDCodePrinter_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+                //foreach (string ptname in PrinterSettings.InstalledPrinters)//获取打印机列表
+                //{
+                //    comboBox1.Items.Add(ptname);
+                //    comboBox1.SelectedIndex = 0;
+                //}
+                reportViewer1.LocalReport.ReportEmbeddedResource = "IDCodePrinter.Report.Report1.rdlc";
+
+                Image img = Encode_EAN_13("12345678");
+                Bitmap imgBit = new Bitmap(img);
+                byte[] imgBytes = BitmapToBytes(imgBit);
+
+                Image img2 = Encode_DM("# 3Q0.915.590.F#131355100#", 5, 10);
+                Bitmap imgBit2 = new Bitmap(img2);
+                byte[] imgBytes2 = BitmapToBytes(imgBit2);
+
+                ReportParameter ReportParam = new ReportParameter("ReportParameter1", Convert.ToBase64String(imgBytes));
+                ReportParameter ReportParam2 = new ReportParameter("ReportParameter2", Convert.ToBase64String(imgBytes2));
+                ReportParameter ReportParam3 = new ReportParameter("ReportParameter3", "77889900");
+
+                reportViewer1.LocalReport.SetParameters(new ReportParameter[] { ReportParam, ReportParam2, ReportParam3 });
+                reportViewer1.RefreshReport();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LocalReport report = new LocalReport();
+            report.ReportPath = @"Report1.rdlc";
+
+            Image img = Encode_EAN_13("12345678");
+            Bitmap imgBit = new Bitmap(img);
+            byte[] imgBytes = BitmapToBytes(imgBit);
+
+            Image img2 = Encode_DM("# 3Q0.915.590.F#131355100#", 5, 10);
+            Bitmap imgBit2 = new Bitmap(img2);
+            byte[] imgBytes2 = BitmapToBytes(imgBit2);
+
+            ReportParameter ReportParam = new ReportParameter("ReportParameter1", Convert.ToBase64String(imgBytes));
+            ReportParameter ReportParam2 = new ReportParameter("ReportParameter2", Convert.ToBase64String(imgBytes2));
+            ReportParameter ReportParam3 = new ReportParameter("ReportParameter3", "77889900");
+            report.SetParameters(new ReportParameter[] { ReportParam, ReportParam2, ReportParam3 });
+
+            report.Refresh();
+
+            string deviceInfo = "<DeviceInfo>" +
+                "  <OutputFormat>EMF</OutputFormat>" +
+                "  <PageWidth>12cm</PageWidth>" +
+                "  <PageHeight>50cm</PageHeight>" +
+                "  <MarginTop>0cm</MarginTop>" +
+                "  <MarginLeft>0cm</MarginLeft>" +
+                "  <MarginRight>0cm</MarginRight>" +
+                "  <MarginBottom>0cm</MarginBottom>" +
+                "</DeviceInfo>";
+            Warning[] warnings;
+            report.Render("Image", deviceInfo, CreateStream, out warnings);//生成数据流
+            Print();//执行打印
+        }
+
+        private List<Stream> m_streams;
+        private Stream CreateStream(string name, string fileNameExtension,
+            Encoding encoding, string mimeType, bool willSeek)
+        {
+            m_streams = new List<Stream>();
+            Stream stream = new MemoryStream();
+            m_streams.Add(stream);
+            return stream;
+        }
+
+        private int m_currentPageIndex;
+        private void Print()
+        {
+            m_currentPageIndex = 0;
+            if (m_streams == null || m_streams.Count == 0)
+                return;
+            PrintDocument printDoc = new PrintDocument();
+            //printDoc.PrinterSettings.PrinterName = "Microsoft Print to PDF";//打印机名称 默认打印机不设
+            if (!printDoc.PrinterSettings.IsValid)
+            {
+                MessageBox.Show("Can't find printer");
+                return;
+            }
+            printDoc.PrintPage += new PrintPageEventHandler(PrintPage);
+            printDoc.Print();
+        }
+
+        private void PrintPage(object sender, PrintPageEventArgs ev)
+        {
+            m_streams[m_currentPageIndex].Position = 0;
+            Metafile pageImage = new Metafile(m_streams[m_currentPageIndex]);
+
+            ev.PageSettings.Landscape = false;//指定是否横向打印
+            ev.Graphics.DrawImage(pageImage, 0, 0);
+            m_streams[m_currentPageIndex].Close();
+            m_currentPageIndex++;
+            ev.HasMorePages = (m_currentPageIndex < m_streams.Count);
+        }
+    }
+}
