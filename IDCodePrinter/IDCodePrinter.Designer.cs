ae66b3be3ff6c2ed86ab28f1468f9a2237f2beac
shen.jz <shen.jz@dt1.biz>
2017-08-03 14:00:56 +0800

    >>2017-08-03_001
    * 增加Code128条码生成方法
    * 标签格式调整
    * 标签内容生成规则调整
    * 增加Logo图片

--- IDCodePrinter/IDCodePrinter.Designer.cs
+++ IDCodePrinter/IDCodePrinter.Designer.cs
@@ -30,29 +30,29 @@
         {
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // reportViewer1
             // 
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "IDCodePrinter.Report.Report1.rdlc";
-            this.reportViewer1.Location = new System.Drawing.Point(266, 12);
+            this.reportViewer1.Location = new System.Drawing.Point(14, 77);
             this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.Size = new System.Drawing.Size(536, 466);
+            this.reportViewer1.Size = new System.Drawing.Size(423, 406);
             this.reportViewer1.TabIndex = 2;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("宋体", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(21, 131);
+            this.button1.Location = new System.Drawing.Point(453, 77);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 74);
             this.button1.TabIndex = 3;
@@ -60,82 +60,27 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(90, 21);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(170, 21);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.Text = "131355100";
-            // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(90, 48);
+            this.textBox2.Location = new System.Drawing.Point(59, 50);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(103, 21);
+            this.textBox2.Size = new System.Drawing.Size(69, 21);
             this.textBox2.TabIndex = 5;
-            this.textBox2.Text = "12345678";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 12);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "二维码序号";
+            this.textBox2.Text = "SVWAP00001";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 51);
+            this.label2.Location = new System.Drawing.Point(12, 53);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.Size = new System.Drawing.Size(41, 12);
             this.label2.TabIndex = 8;
-            this.label2.Text = "条形码序号";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 78);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 12);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "生产编号";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(19, 105);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 12);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "产品型号";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "BATTypPHEV",
-            "BATTypBEV"});
-            this.comboBox1.Location = new System.Drawing.Point(90, 102);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(103, 20);
-            this.comboBox1.TabIndex = 12;
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.CustomFormat = "ddMMyyyy";
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(90, 75);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(103, 21);
-            this.dateTimePicker1.TabIndex = 13;
+            this.label2.Text = "流水号";
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("宋体", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button2.Location = new System.Drawing.Point(118, 131);
+            this.button2.Location = new System.Drawing.Point(453, 169);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 74);
             this.button2.TabIndex = 14;
@@ -143,20 +88,71 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "软件版本";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(193, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 21);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.Text = "0001";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(231, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "硬件版本";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(290, 50);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(26, 21);
+            this.textBox3.TabIndex = 17;
+            this.textBox3.Text = "001";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 12);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "二维码/条形码";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(101, 23);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(241, 21);
+            this.textBox4.TabIndex = 19;
+            this.textBox4.Text = "5KE915588F____#037______#653709246#288";
+            // 
             // IDCodePrinter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(834, 495);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(546, 495);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.reportViewer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -173,15 +169,15 @@
         #endregion
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- IDCodePrinter/IDCodePrinter.cs
+++ IDCodePrinter/IDCodePrinter.cs
@@ -13,6 +13,8 @@ using System.IO;
 using System.Drawing.Printing;
 using ByteMatrix = com.google.zxing.common.ByteMatrix;
 using EAN8Writer = com.google.zxing.oned.EAN8Writer;
+using Code39Writer = com.google.zxing.oned.Code39Writer;
+using MultiFormatWriter = com.google.zxing.MultiFormatWriter;
 using com.google.zxing;
 using DataMatrix.net;
 using S7;
@@ -51,13 +53,30 @@ namespace IDCodePrinter
             return bm;
         }
 
-        public static Bitmap Encode_EAN_13(string content)
+        public static Bitmap Encode_EAN_8(string content)
         {
             EAN8Writer ean8w = new EAN8Writer();
             ByteMatrix byteMatrix = ean8w.encode(content, BarcodeFormat.EAN_8, 300, 200);
             Bitmap bitmap = ByteMatrixToBitmap(byteMatrix);
 
             return bitmap;
+        }
+
+        public static Bitmap Encode_Code_39(string content)
+        {
+            Code39Writer ean8w = new Code39Writer();
+            
+            ByteMatrix byteMatrix = ean8w.encode(content, BarcodeFormat.CODE_39, 500, 38);
+            Bitmap bitmap = ByteMatrixToBitmap(byteMatrix);
+
+            return bitmap;
+        }
+
+        public static Bitmap Encode_Code_128(string content)
+        {
+            Code128 _Code = new Code128();
+            //_Code.ValueFont = new Font("宋体", 20);
+            return _Code.GetCodeImage(content, Code128.Encode.Code128A);
         }
 
         public static Bitmap ByteMatrixToBitmap(ByteMatrix matrix)
@@ -98,30 +117,31 @@ namespace IDCodePrinter
 
         private void IDCodePrinter_Load(object sender, EventArgs e)
         {
-            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
-            comboBox1.SelectedIndex = 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            DateTime datetime = DateTime.Now;
             LocalReport report = new LocalReport();
             report.ReportPath = @".\Report\Report1.rdlc";
 
-            Image img = Encode_EAN_13(textBox2.Text);
+            Image img = Encode_Code_39(textBox1.Text + textBox3.Text);//Encode_EAN_8(textBox2.Text);
             Bitmap imgBit = new Bitmap(img);
             byte[] imgBytes = BitmapToBytes(imgBit);
 
-            Image img2 = Encode_DM("# 3Q0.915.590.F#" + textBox1.Text + "#", 5, 10);
+            Image img2 = Encode_DM(textBox4.Text + textBox2.Text + "_______#___#" + datetime.ToString("ddMMyyyyHHmmss") + "#", 5, 10);
             Bitmap imgBit2 = new Bitmap(img2);
             byte[] imgBytes2 = BitmapToBytes(imgBit2);
 
             ReportParameter ReportParam = new ReportParameter("ReportParameter1", Convert.ToBase64String(imgBytes));
             ReportParameter ReportParam2 = new ReportParameter("ReportParameter2", Convert.ToBase64String(imgBytes2));
-            ReportParameter ReportParam3 = new ReportParameter("ReportParameter3", textBox2.Text);
-            ReportParameter ReportParam4 = new ReportParameter("ReportParameter4", dateTimePicker1.Text);
-            ReportParameter ReportParam5 = new ReportParameter("ReportParameter5", comboBox1.SelectedItem.ToString());
+            ReportParameter ReportParam3 = new ReportParameter("ReportParameter3", textBox4.Text);
+            ReportParameter ReportParam4 = new ReportParameter("ReportParameter4", datetime.ToString("ddMMyyyy"));
+            ReportParameter ReportParam5 = new ReportParameter("ReportParameter5", textBox1.Text);
+            ReportParameter ReportParam6 = new ReportParameter("ReportParameter6", textBox3.Text);
+            ReportParameter ReportParam7 = new ReportParameter("ReportParameter7", textBox2.Text);
             report.SetParameters(new ReportParameter[] { ReportParam, ReportParam2,
-                    ReportParam3, ReportParam4, ReportParam5 });
+                    ReportParam3, ReportParam4, ReportParam5, ReportParam6, ReportParam7 });
 
             report.Refresh();
 
@@ -129,10 +149,10 @@ namespace IDCodePrinter
                 "  <OutputFormat>EMF</OutputFormat>" +
                 "  <PageWidth>9.5cm</PageWidth>" +
                 "  <PageHeight>9.5cm</PageHeight>" +
-                "  <MarginTop>0cm</MarginTop>" +
-                "  <MarginLeft>0cm</MarginLeft>" +
-                "  <MarginRight>0cm</MarginRight>" +
-                "  <MarginBottom>0cm</MarginBottom>" +
+                "  <MarginTop>0.1cm</MarginTop>" +
+                "  <MarginLeft>0.1cm</MarginLeft>" +
+                "  <MarginRight>0.1cm</MarginRight>" +
+                "  <MarginBottom>0.1cm</MarginBottom>" +
                 "</DeviceInfo>";
             Warning[] warnings;
             //report.Render("Image", deviceInfo, CreateStream, out warnings);//生成数据流
@@ -192,7 +212,9 @@ namespace IDCodePrinter
             Metafile pageImage = new Metafile(m_streams[m_currentPageIndex]);
 
             ev.PageSettings.Landscape = false;//指定是否横向打印
-            ev.Graphics.DrawImage(pageImage, 0, 0);
+            Rectangle destRect = new Rectangle(0, 0, pageImage.Width, pageImage.Height);//设置打印区域大小
+            ev.Graphics.DrawImage(pageImage, destRect);
+            //ev.Graphics.DrawImage(pageImage, 0, 0);
             m_streams[m_currentPageIndex].Close();
             m_currentPageIndex++;
             ev.HasMorePages = (m_currentPageIndex < m_streams.Count);
@@ -202,6 +224,7 @@ namespace IDCodePrinter
         {
             try
             {
+                DateTime datetime = DateTime.Now;
                 //foreach (string ptname in PrinterSettings.InstalledPrinters)//获取打印机列表
                 //{
                 //    comboBox1.Items.Add(ptname);
@@ -210,22 +233,24 @@ namespace IDCodePrinter
                 //reportViewer1.LocalReport.ReportEmbeddedResource = "IDCodePrinter.Report.Report1.rdlc";
                 reportViewer1.LocalReport.ReportPath = @".\Report\Report1.rdlc";
 
-                Image img = Encode_EAN_13(textBox2.Text);
+                Image img = Encode_Code_39(textBox1.Text + textBox3.Text); //Encode_EAN_8(textBox2.Text);
                 Bitmap imgBit = new Bitmap(img);
                 byte[] imgBytes = BitmapToBytes(imgBit);
 
-                Image img2 = Encode_DM("# 3Q0.915.590.F#" + textBox1.Text + "#", 5, 10);
+                Image img2 = Encode_DM(textBox4.Text + textBox2.Text + "_______#___#" + datetime.ToString("ddMMyyyyHHmmss") + "#", 5, 10);
                 Bitmap imgBit2 = new Bitmap(img2);
                 byte[] imgBytes2 = BitmapToBytes(imgBit2);
 
                 ReportParameter ReportParam = new ReportParameter("ReportParameter1", Convert.ToBase64String(imgBytes));
                 ReportParameter ReportParam2 = new ReportParameter("ReportParameter2", Convert.ToBase64String(imgBytes2));
-                ReportParameter ReportParam3 = new ReportParameter("ReportParameter3", textBox2.Text);
-                ReportParameter ReportParam4 = new ReportParameter("ReportParameter4", dateTimePicker1.Text);
-                ReportParameter ReportParam5 = new ReportParameter("ReportParameter5", comboBox1.SelectedItem.ToString());
+                ReportParameter ReportParam3 = new ReportParameter("ReportParameter3", textBox4.Text);
+                ReportParameter ReportParam4 = new ReportParameter("ReportParameter4", datetime.ToString("ddMMyyyy"));
+                ReportParameter ReportParam5 = new ReportParameter("ReportParameter5", textBox1.Text);
+                ReportParameter ReportParam6 = new ReportParameter("ReportParameter6", textBox3.Text);
+                ReportParameter ReportParam7 = new ReportParameter("ReportParameter7", textBox2.Text);
 
                 reportViewer1.LocalReport.SetParameters(new ReportParameter[] { ReportParam, ReportParam2,
-                    ReportParam3, ReportParam4, ReportParam5 });
+                    ReportParam3, ReportParam4, ReportParam5, ReportParam6, ReportParam7 });
                 reportViewer1.RefreshReport();
             }
             catch (Exception ex)
